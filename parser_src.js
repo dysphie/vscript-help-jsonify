a1471dcb3a263eb05d7c1a4919b89a5a1407c370
Dysphie <dysphie@gmail.com>
2022-06-17 23:52:57 -0300

    add support for '<unknown>' identifiers

--- parser_src.js
+++ parser_src.js
@@ -9,26 +9,26 @@
 }
 
 start
-	= (FnBlock / ClassMember / Hook / Constant / Enum / Class)*
+    = (FnBlock / ClassMember / Hook / Constant / Enum / Class)*
 
 DescriptionShared
-	= 'Description:' _ desc:char+ { return desc.join(""); }
+    = 'Description:' _ desc:char+ { return desc.join(""); }
     
 HookClass 
-	= cls:(Identifier) _ "->" _ { return cls }
+    = cls:(Identifier) _ "->" _ { return cls }
     
 MemberClass
-	= cls:(Identifier) separator:"." { return cls }
+    = cls:(Identifier) separator:"." { return cls }
     
 FnClass
-	= cls:(Identifier) separator:"::" { return cls }
+    = cls:(Identifier) separator:"::" { return cls }
 
 FnBlock
-	= 'Function:' _ FnClass? Identifier ___
+    = 'Function:' _ FnClass? Identifier ___
     'Signature:' _ ret:RetVal? _ cls:FnClass? method:Identifier '(' args:(ParamList)? ')' ___
     desc: DescriptionShared? ___
     {
-    	return {
+        return {
         "block_type": "function",
         "method": method,
         "ret": ret,
@@ -50,14 +50,14 @@ IntegerValue "integer"
      = "-"? digits:[0-9]+ { return parseInt(digits.join(""), 10); }
 
 VectorValue "vector"
-	= "Vector(" _ x:FloatValue _ "," _ y:FloatValue _ "," _ z:FloatValue _ ")" { return text(); }
+    = "Vector(" _ x:FloatValue _ "," _ y:FloatValue _ "," _ z:FloatValue _ ")" { return text(); }
 
 Constant
-	= 'Constant:' _ ("(" _ VarType _ ")")? _ from:(Identifier".")? id:Identifier ___
+    = 'Constant:' _ ("(" _ VarType _ ")")? _ from:(Identifier".")? id:Identifier ___
    'Value:' _ value:ConstantValue _ "(" type:VarType ")" ___
     desc: DescriptionShared? ___
     {
-    	return {
+        return {
         "block_type": "constant",
         "name": id,
         "type": type,
@@ -68,25 +68,25 @@ Constant
     }
     
 ClassMember
-	= 'Member:' _ MemberClass Identifier ___
+    = 'Member:' _ MemberClass Identifier ___
     'Signature:' _ ret:VarType _ cls:MemberClass id:Identifier ___
     desc: DescriptionShared? ___ 
     {
-    	return {
-        	"block_type": "class_member",
+        return {
+            "block_type": "class_member",
             "name": id,
-        	"class": cls,
+            "class": cls,
             "description" : desc
         }
     }
     
 Hook
-	= 'Hook:' _ HookClass  Identifier ___
+    = 'Hook:' _ HookClass  Identifier ___
     'Signature:'  _ ret:VarType _ cls:HookClass id:Identifier '(' args:(ParamList)? ')' ___
     desc: DescriptionShared? ___
     {
-    	return {
-       		"block_type": "hook",
+        return {
+            "block_type": "hook",
             "name": id,
             "class": cls,
             "description": desc,
@@ -99,33 +99,33 @@ newline = '\n' / '\r' '\n'?
 char = [^\n\r]
     
 VarType
-	= name:[a-zA-Z_]+ { return name.join("") } 
+    = name:[a-zA-Z_<>]+ { return name.join("") } 
     
 VarTypeAndName
-	= type:VarType _ ("[" name:$(char+) "]")
+    = type:VarType _ ("[" name:$(char+) "]")
     { return {
         "name": name,
-    	"type": type
+        "type": type
         }
     }
     
 Identifier
-	= name:[a-zA-Z0-9_]+ { return name.join("") }
+    = name:[a-zA-Z0-9_<>]+ { return name.join("") }
     
 ParameterName =  "[" _ name:Identifier _ "]" { return name }
 
-Parameter	
-	= type:Identifier _ name:ParameterName? { 
+Parameter   
+    = type:Identifier _ name:ParameterName? { 
     return { "type": type, "name": name } }
     
 _ 'Whitespace'
     = [ \t\r]*
     
 __ 'MandatoryWhitespace'
-	= [ \t\r]+
+    = [ \t\r]+
     
 ___ 'NewLine'
-	= [ \n]*
+    = [ \n]*
 
 ParamList
   = head:Parameter tail:(_ "," _ Parameter)* {
@@ -133,17 +133,17 @@ ParamList
     }
     
 Separator
-	= '=====================================' ___
+    = '=====================================' ___
      
 Enum
-	= Separator
+    = Separator
     'Enum:' _ id:Identifier ___
     'Elements:' _ count:IntegerValue ___
     desc: DescriptionShared? ___
     Separator
     {
-    	return {
-       		"block_type": "enum",
+        return {
+            "block_type": "enum",
             "name": id,
             "size": count,
             "description": desc
@@ -152,15 +152,15 @@ Enum
 
 
 Class 
-	=
+    =
     Separator
     'Class:' _ id:Identifier ___
     'Base:' _ base:Identifier? ___
     desc: DescriptionShared? ___ 
     Separator
     {
-    	return {
-       		"block_type": "class",
+        return {
+            "block_type": "class",
             "class": id,
             "base": base,
             "description": desc
@@ -168,4 +168,4 @@ Class
     }
      
 RetVal
-	= type:VarType __ { return type }
+    = type:VarType __ { return type }
